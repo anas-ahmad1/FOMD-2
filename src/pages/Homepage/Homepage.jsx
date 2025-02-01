882f0671c9869632637f0ef1c72d1cc2eaa8e261
pwtrace <pwtrace0@gmail.com>
2025-02-01 15:50:46 +0500

    Add What we do view for Homepage with responsiveness

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import "./Homepage.css";
 import HomeMainView from "../../components/HomeMainView/HomeMainView";
+import WhatWeDo from "../../components/WhatWeDo/WhatWeDo";
 
 const Homepage = () => {
   return (
     <>
       <div className="home-container">
         <HomeMainView/>
+        <WhatWeDo/>
       </div>
     </>
   );

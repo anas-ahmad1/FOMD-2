e51607ec1603394498ba02615cf10c5855f5311f
Anas Ahmad <anasahmadcs7@gmail.com>
2025-02-02 16:38:01 +0500

    Add transitions component for Homepage

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -3,13 +3,16 @@ import "./Homepage.css";
 import HomeMainView from "../../components/HomeMainView/HomeMainView";
 import WhatWeDo from "../../components/WhatWeDo/WhatWeDo";
 import HowDoesItWork from "../../components/HowDoesItWork/HowDoesItWork";
+import PageTransition from "../../components/PageTransition/PageTransition";
 
 const Homepage = () => {
   return (
     <>
       <div className="home-container">
         <HomeMainView/>
+        <PageTransition transitionDirection="l2d"/>
         <WhatWeDo/>
+        <PageTransition transitionDirection="d2l"/>
         <HowDoesItWork/>
       </div>
     </>

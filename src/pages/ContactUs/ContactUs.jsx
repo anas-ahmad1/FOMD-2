fc303ca9193245fdf7a8493a5678fbe26c8741e0
Anas Ahmad <anasahmadcs7@gmail.com>
2025-02-07 22:30:17 +0500

    Add navigation and Create blank contact us page

---
+++ src/pages/ContactUs/ContactUs.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./ContactUs.css";
+
+const ContactUs = () => {
+
+  return (
+    <>
+      <div className="contact-us-container">
+        
+      </div>
+    </>
+  );
+};
+
+export default ContactUs;

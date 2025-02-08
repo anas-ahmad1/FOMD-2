e436a10fb13b49215d303cd8de925585bdbbffda
Anas Ahmad <anasahmadcs7@gmail.com>
2025-02-08 22:22:49 +0500

    Add active page styles for navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import logo from "../../assets/logo.jpeg";
+import { useLocation } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
 
+  const location = useLocation();
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => {
@@ -26,11 +28,11 @@ const Navbar = () => {
 
           <div className={`nav-container-right ${isOpen ? "open" : ""}`}>
             <div className="close-btn" onClick={toggleMenu}>&times;</div>
-            <div className="nav-item"><a className="nav-link" href="/">Home</a></div>
-            <div className="nav-item"><a className="nav-link" href="/gallery">Gallery</a></div>
-            <div className="nav-item"><a className="nav-link" href="/services">Services</a></div>
-            <div className="nav-item"><a className="nav-link" href="#">Blogs</a></div>
-            <div className="nav-item nav-contact"><a className="nav-link" href="/contactus">Contact Us</a></div>
+            <div className={`nav-item ${location.pathname === "/" ? "active" : ""}`}><a className="nav-link" href="/">Home</a></div>
+            <div className={`nav-item ${location.pathname === "/gallery" ? "active" : ""}`}><a className="nav-link" href="/gallery">Gallery</a></div>
+            <div className={`nav-item ${location.pathname === "/services" ? "active" : ""}`}><a className="nav-link" href="/services">Services</a></div>
+            <div className={`nav-item ${location.pathname === "/blogs" ? "active" : ""}`}><a className="nav-link" href="/blogs">Blogs</a></div>
+            <div className={`nav-item nav-contact ${location.pathname === "/contactus" ? "contact-active" : ""}`}><a className="nav-link" href="/contactus">Contact Us</a></div>
           </div>
       </nav>
       </div>

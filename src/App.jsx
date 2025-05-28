68a2b6f40d670c37dca86d64431b70ab4b95cc4b
Anas Ahmad <anasahmadcs7@gmail.com>
2025-05-28 22:11:09 +0500

    Fix minor issues

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Gallery from "./pages/Gallery/Gallery.jsx";
 import ContactUs from "./pages/ContactUs/ContactUs.jsx";
 import Services from "./pages/Services/Services.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer/Footer.jsx"
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
             <Route path="/blog-1" element={<Blog1 />} />
             <Route path="/blog-2" element={<Blog2 />} />
           </Routes>
+          <Footer />
         </Router>
       </div>
     </>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
 import logo from "../../assets/logo.jpeg";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
-
   const location = useLocation();
   const [isOpen, setIsOpen] = useState(false);
 
@@ -12,29 +11,58 @@ const Navbar = () => {
     setIsOpen(!isOpen);
   };
 
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
   return (
     <>
-    <div className="main-container-nav">
-      <nav className="nav-container">
-        <div className="nav-container-left">
-          <a href="/">
-            <img src={logo} className="nav-logo" alt="" />
-          </a>
-        </div>
+      <div className="main-container-nav">
+        <nav className="nav-container">
+          <div className="nav-container-left">
+            <Link to="/" onClick={closeMenu}>
+              <img src={logo} className="nav-logo" alt="Logo" />
+            </Link>
+          </div>
 
-        <div className="hamburger" onClick={toggleMenu}>
+          <div className="hamburger" onClick={toggleMenu}>
             &#9776;
           </div>
 
           <div className={`nav-container-right ${isOpen ? "open" : ""}`}>
             <div className="close-btn" onClick={toggleMenu}>&times;</div>
-            <div className={`nav-item ${location.pathname === "/" ? "active" : ""}`}><a className="nav-link" href="/">Home</a></div>
-            <div className={`nav-item ${location.pathname === "/gallery" ? "active" : ""}`}><a className="nav-link" href="/gallery">Gallery</a></div>
-            <div className={`nav-item ${location.pathname === "/services" ? "active" : ""}`}><a className="nav-link" href="/services">Services</a></div>
-            <div className={`nav-item ${location.pathname === "/blogs" ? "active" : ""}`}><a className="nav-link" href="/blogs">Blogs</a></div>
-            <div className={`nav-item nav-contact ${location.pathname === "/contactus" ? "contact-active" : ""}`}><a className="nav-link" href="/contactus">Contact Us</a></div>
+
+            <div className={`nav-item ${location.pathname === "/" ? "active" : ""}`}>
+              <Link className="nav-link" to="/" onClick={closeMenu}>
+                Home
+              </Link>
+            </div>
+
+            <div className={`nav-item ${location.pathname === "/gallery" ? "active" : ""}`}>
+              <Link className="nav-link" to="/gallery" onClick={closeMenu}>
+                Gallery
+              </Link>
+            </div>
+
+            <div className={`nav-item ${location.pathname === "/services" ? "active" : ""}`}>
+              <Link className="nav-link" to="/services" onClick={closeMenu}>
+                Services
+              </Link>
+            </div>
+
+            <div className={`nav-item ${location.pathname === "/blogs" ? "active" : ""}`}>
+              <Link className="nav-link" to="/blogs" onClick={closeMenu}>
+                Blogs
+              </Link>
+            </div>
+
+            <div className={`nav-item nav-contact ${location.pathname === "/contactus" ? "contact-active" : ""}`}>
+              <Link className="nav-link" to="/contactus" onClick={closeMenu}>
+                Contact Us
+              </Link>
+            </div>
           </div>
-      </nav>
+        </nav>
       </div>
     </>
   );

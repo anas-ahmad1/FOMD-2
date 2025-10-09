2d20583cc691defe097b332d1aeb66304295054f
Anas Ahmad <anasahmadcs7@gmail.com>
2025-10-10 01:32:50 +0500

    use different video for mobile

--- src/components/DashboardMainSection/DashboardMainSection.jsx
+++ src/components/DashboardMainSection/DashboardMainSection.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "./DashboardMainSection.css";
 import bgVideo from "../../assets/bg-video2.mp4";
+import bgVideoMobile from "../../assets/bg-video-mobile.mp4";
 
 const DashboardMainSection = () => {
+  const isMobile = window.innerWidth <= 768;
+  const videoSrc = isMobile ? bgVideoMobile : bgVideo;
+
   return (
     <div className="hero-video-container">
       <video className="hero-video" autoPlay loop muted playsInline>
-        <source src={bgVideo} type="video/mp4" />
+        <source src={videoSrc} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       <div className="overlay"></div>
